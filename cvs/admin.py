11ddf768c04afdbfa81ca0a5f1c6b64f30ed9a58
Your Name <cogoca@gmail.com>
2024-04-12 10:28:48 +0200

    segundo del dia

--- cvs/admin.py
+++ cvs/admin.py
@@ -1,3 +1,10 @@
-from django.contrib import admin
+
 
 # Register your models here.
+from django.contrib import admin
+
+from .models import Experiencia
+from .models import Persona
+
+admin.site.register(Experiencia)
+admin.site.register(Persona)
--- cvs/models.py
+++ cvs/models.py
@@ -9,12 +9,17 @@ class Persona(models.Model):
     email= models.CharField(max_length=100)
     linkedin= models.CharField(max_length=100)
     pub_date = models.DateTimeField("fecha update")
+    def __str__(self):
+        return self.nombre + " " + self.apellidos
 
 
 class Experiencia(models.Model):
     experiencia= models.ForeignKey(Persona, on_delete=models.CASCADE)
     empresa = models.CharField(max_length=200)
     puesto = models.CharField(max_length=200)
+    descripcion = models.CharField(max_length=400, default='')
     anios = models.IntegerField(default=0)
-    inicio_date = models.DateTimeField("inicio trabajo")
-    final_date = models.DateTimeField("final trabajo")
+    inicio_date = models.DateField("inicio trabajo")
+    final_date = models.DateField("final trabajo")
+    def __str__(self):
+        return self.puesto + "/ " + self.empresa
